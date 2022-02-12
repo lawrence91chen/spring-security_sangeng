dde25560e584e65cc0f5462acceb940917b71210
lawrence_chen <lawrence91chen@gmail.com>
2022-02-12 17:40:24 +0800

    feat: quick-start

---
+++ quick-start/src/main/java/com/example/BootApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot 啟動類。
+ */
+@SpringBootApplication
+public class BootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+}
